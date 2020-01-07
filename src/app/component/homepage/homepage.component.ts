7982ff781a4fe6767fe9204aa3fda8975d1cefad
shikha <shikha.baskandi@quovantis.com>
2020-01-07 18:31:24 +0530

    Gsap implementation with scrollmagic

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {TimelineMax} from 'gsap';
+import { gsap } from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import '../../../common/animation.gsap';
+import '../../../common/debug.addIndicators';
 
 @Component({
   selector: 'app-homepage',
@@ -7,21 +10,72 @@ import {TimelineMax} from 'gsap';
   styleUrls: ['./homepage.component.css']
 })
 
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  ngOnInit() {
+    this.animation();
+  }
 
-  clickMe() {
-    // debugger;
-    const tl = new TimelineMax();
-    tl
-      .to('#para', 1, {
-      transformOrigin: 'center',
-      scale: 1.2,
-      ease: 'Expo.easeInOut'
-    })
-    .fromTo('.jetBubble', {
-        opacity: 0,
-      }, {
+  private animation() {
+    // @ts-ignore
+    const timeline = new gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: 'ease',
+        transformOrigin: 'center'
+      }
+    });
+
+    timeline
+      .from('.pulseSVG', {
         opacity: 1,
+      })
+      .from('.astronaut', {
+        duration: 0.1,
+        y: '+=5',
+      }, '-=1')
+      .from('.jetBubble', {
+        duration: 0.2,
+        opacity: 1,
+        scale: 0.8,
+      })
+      .from('.pulse', {
+        opacity: 0,
+        stagger: 0.2,
+      })
+      .from('#speedLine0', {
+        y: '-=1000',
+      })
+      .from('#speedLine1', {
+        y: '-=1000',
+      })
+      .from('#speedLine2', {
+        y: '-=1000',
+      })
+      .from('#speedLine3', {
+        y: '-=1000',
+      })
+      .from('#speedLine4', {
+        y: '-=1000',
+      })
+      .from('#speedLine5', {
+        y: '-=1000',
+      })
+      .from('#speedLine6', {
+        y: '-=1000',
       });
+
+    // ScrollMagic
+    // @ts-ignore
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+      triggerElement: '.wrapper',
+      triggerHook: 0,
+      duration: '100%',
+      offset: 100
+    })
+      .setPin('.wrapper')
+      .setTween(timeline)
+      .addTo(controller)
+      .addIndicators();
   }
 }
